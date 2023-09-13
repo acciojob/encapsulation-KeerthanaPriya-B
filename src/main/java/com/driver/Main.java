24510c18829d4c632fe0d310b0deb5770a70eb8a
KeerthanaPriya-B <priyakeerthana21@gmail.com>
2023-09-13 21:07:49 +0530

    implemented encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+
+        //since name is private, we cannot access it directly
+        //obj.name = "AccioJob";
+
+        obj.setName("AccioJob");
+        System.out.print(obj.getName());
+    }
 }
